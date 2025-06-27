64a75dad5b8e6fa83290fd27194f1194b7453f79
Bilal Kashan <127339814+bilalkashan@users.noreply.github.com>
2025-06-28 02:52:06 +0500

    Fix import path for Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { Routes, Route, useNavigate, useLocation, Navigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import AllTasks from "./pages/AllTasks";
 import IncompleteTasks from "./pages/IncompleteTasks";
 import CompleteTasks from "./pages/CompleteTasks";
